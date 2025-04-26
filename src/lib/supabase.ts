2359eafbc8b296656800cab223871a5a5fe1919f
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2025-04-26 23:10:24 +0500

    tryon ui

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kgzyctbjbwetriagenqo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnenljdGJqYndldHJpYWdlbnFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4NjA5NzcsImV4cCI6MjA1NTQzNjk3N30.PCFgLwhwOGKe6OpUqgMMafN4_Gg6sUaTDY_278IR6A8';
+const supabaseUrl = 'https://vhhmrqpmaszrahlfyuif.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoaG1ycXBtYXN6cmFobGZ5dWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2OTA0MzcsImV4cCI6MjA2MTI2NjQzN30.-oDu3wltoypI3OidiIrOJj-YChJ7-CgnKAIpTrHGb-Q';
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
